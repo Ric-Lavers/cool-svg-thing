f1cc54d1ee96a46b489e0b1b21599f2bcecf4ca4
Ric-Lavers <ric_lavers@outlook.com>
2018-04-13 16:31:58 +1000

    cool

--- src/FullScreenSVG.jsx
+++ src/FullScreenSVG.jsx
@@ -11,26 +11,35 @@ class FullScreenSVG extends Component {
     innerHeight:0, 
     innerWidth: 0,
     polylinePoints: [],
-    polylinePointsString:[]
+    polylinePointsString:[],
+    intervals:4,
+    polyline: true
   }
 
   componentDidMount(){
     let { innerHeight, innerWidth } = window
     this.setState({ innerHeight, innerWidth })
+    document.addEventListener('keypress', (event) => {
+      this.handleKeyPress(event)
+    })
+  }
+  componentWillUnmount(){
+    document.removeEventListener('keypress', (event) => {
+      this.handleKeyPress(event)
+    })
   }
 
   handleClick = (event) => {
     let { clientX, clientY } = event
     let points = this.state.polylinePoints
     let current = [clientX, clientY]
-
     if(points.length >5){
     let last = [points[points.length-2], points[points.length-1]] 
     let beforeLast = [points[points.length-4], points[points.length-3]] 
 
     const findIntervals = (start, end) => {
-      let interval = (start -end)/ 4
-      const array = new Array(5).fill(0).map( (val, i) => val = start-(i*interval) )
+      let interval = (start -end)/ this.state.intervals
+      const array = new Array(this.state.intervals+1).fill(0).map( (val, i) => val = start-(i*interval) )
       return array
     }
 
@@ -40,31 +49,41 @@ class FullScreenSVG extends Component {
     let pattern = intervalX.map( (v,i) => [intervalX[i], intervalY[i], clientX, clientY ]  )
 
     let flat = [].concat.apply([], pattern)
-    this.setState({ polylinePointsString:flat })
-    console.log(flat)
+    let { polylinePointsString } = this.state
+    polylinePointsString.push(flat)
+    this.setState({ polylinePointsString })
+
     }
 
     points.push(clientX, clientY)
     this.setState({ clientX, clientY, polylinePoints:points })
-    console.log(  points)
   }
   stringArt = (x, m, c)=>{
     let y = m * x + c
 
+  }
 
+  handleKeyPress = (event) => {
+    let intervals  = parseInt(event.key)
+    event.key === "p" && this.setState({ polyline: !this.state.polyline })
+    let allow = [1,2,3,4,5,6,7,8,9]
+    allow.includes(intervals) && this.setState({ intervals })
   }
 
   render (){
-    let { innerHeight, innerWidth, polylinePoints ,polylinePointsString } = this.state
+    let {polyline, innerHeight, innerWidth, polylinePoints ,polylinePointsString } = this.state
     console.log('polylinePoints', polylinePoints )
     return( 
-      <div onClick={this.handleClick}>
-      {/* this.state.polylinePoints */}
+      <div onClick={this.handleClick} > 
         <svg key={this.k++} viewBox={`0 0 ${innerWidth} ${innerHeight}`} >
           <polyline points={polylinePoints}
           style={{fill:"none",stroke:"black",strokeWidth:3}} />
-          <polyline points={polylinePointsString}
-          style={{fill:"none",stroke:"black",strokeWidth:3}} />
+          {polyline
+            ?<polyline points={polylinePointsString}
+              style={{fill:"none",stroke:"black",strokeWidth:3}} />
+            :<polygon points={polylinePointsString}
+            style={{fill:"none",stroke:"black",strokeWidth:3}} />
+        }
         </svg>
       </div>
     )
