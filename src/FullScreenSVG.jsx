08353428910d460584ce54e2b43bfbd1ad4caf9a
Ric-Lavers <ric_lavers@outlook.com>
2018-04-14 15:24:44 +1000

    draw animation -- works on codepen not browser???

--- src/FullScreenSVG.jsx
+++ src/FullScreenSVG.jsx
@@ -4,6 +4,7 @@ class FullScreenSVG extends Component {
   constructor(props){
     super(props)
     this.k = 0
+    this.path = React.createRef()
   }
   state = {
     clientX: null,
@@ -14,8 +15,9 @@ class FullScreenSVG extends Component {
     polylinePointsString:[],
     intervals:4,
     polyline: true,
-    blackWhite: "black",
-    fill:false
+    blackWhite: "white",
+    fill:false,
+    animationName: null,
   }
 
   componentDidMount(){
@@ -29,6 +31,7 @@ class FullScreenSVG extends Component {
     document.removeEventListener('keypress', (event) => {
       this.handleKeyPress(event)
     })
+
   }
 
   handleClick = (event) => {
@@ -47,9 +50,12 @@ class FullScreenSVG extends Component {
 
     const intervalX = findIntervals(beforeLast[0], last[0])
     const intervalY = findIntervals(beforeLast[1], last[1])
+    const intervalCurrentX = findIntervals(last[0], current[0])
+    const intervalCurrentY = findIntervals(last[1], current[1])
     
     let pattern = intervalX.map( (v,i) => [intervalX[i], intervalY[i], clientX, clientY ]  )
-
+    // let pattern = intervalX.map( (v,i) => [intervalX[i], intervalY[i], intervalCurrentX[i], intervalCurrentY[i] ]  )
+    
     let flat = [].concat.apply([], pattern)
     let { polylinePointsString } = this.state
     polylinePointsString.push(flat)
@@ -62,7 +68,26 @@ class FullScreenSVG extends Component {
   }
   stringArt = (x, m, c)=>{
     let y = m * x + c
+  }
 
+  getPathLength = () => {
+    const path = document.getElementById('drawPolyline')
+    const length = path.getTotalLength()
+
+    
+    let styleSheet = document.styleSheets[0];
+    let animationName = `draw`;
+    let keyframes =
+    `@-webkit-keyframes ${animationName} {
+       to{
+        stroke-dashoffset: ${length};
+       }
+    }`;
+    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+
+    this.setState({ animationName, pathLength: length })
+    let styleSheet2 = document.styleSheets[0];
+    console.log("getPathLength", styleSheet, styleSheet2)
   }
 
   handleKeyPress = (event) => {
@@ -74,21 +99,30 @@ class FullScreenSVG extends Component {
         ?"white"
         :"black"
       }))
+    event.key === "a" && this.getPathLength()
     let allow = [1,2,3,4,5,6,7,8,9]
     allow.includes(intervals) && this.setState({ intervals })
   }
 
   render (){
-    let { fill, blackWhite, polyline, innerHeight, innerWidth, polylinePoints ,polylinePointsString } = this.state
+    let { pathLength, animationName, fill, blackWhite, polyline, innerHeight, innerWidth, polylinePoints ,polylinePointsString } = this.state
     let fillColor = fill ? '#111':'none'
+    let drawPath = animationName
+      ?{
+        strokeDasharray: 1000,
+        animation: `${animationName} 2s linear`,
+        fill:fillColor, stroke:blackWhite, strokeWidth:2
+      }:{fill:fillColor, stroke:blackWhite, strokeWidth:2}
+
     return( 
       <div onClick={this.handleClick} > 
-        <svg key={this.k++} viewBox={`0 0 ${innerWidth} ${innerHeight}`} >
+        <svg key={this.k++} 
+        style={{backgroundColor:'#222'}} 
+        viewBox={`0 0 ${innerWidth} ${innerHeight}`} >
           <polyline points={polylinePoints}
-          style={{fill:"none",stroke:"black",strokeWidth:3}} />
+          style={{fill:"none",stroke:blackWhite,strokeWidth:3}} />
           {polyline
-            ?<polyline points={polylinePointsString}
-              style={{fill:fillColor, stroke:blackWhite, strokeWidth:2}} />
+            ?<polyline id="drawPolyline" style={drawPath} points={polylinePointsString} ref={this.path} />
             :<polygon points={polylinePointsString}
             style={{fill:fillColor, stroke:blackWhite, strokeWidth:2, fillRule:'evenodd'}} />
         }
